ae0dc891051b6a2f7627032911b141271820b92c
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2023-10-08 17:46:47 +0200

    Contextual type

--- output/context.go
+++ output/context.go
@@ -4,6 +4,12 @@ import "context"
 
 type printerKey struct{}
 
+// Contextual is an interface for types that can provide a context.Context.
+type Contextual interface {
+	// Context returns the context.Context.
+	Context() context.Context
+}
+
 // PrinterFrom returns the Printer from the context, or the default printer if
 // none is set. The default printer is OsPrinter.
 func PrinterFrom(ctx context.Context) Printer {
@@ -21,4 +27,10 @@ func WithContext(ctx context.Context, p Printer) context.Context {
 
 func defaultPrinter() Printer {
 	return OsPrinter
+}
+
+// ContextualPrinter is a type that embeds a Contextual and a Printer.
+type ContextualPrinter struct {
+	Contextual
+	Printer
 }
